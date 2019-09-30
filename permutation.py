554a2ff2c2849751c90bd64864be10909a65180c
shu6725 <shugo0203@gmail.com>
2019-09-30 11:14:36 +0000

    sep

--- permutation.py
+++ permutation.py
@@ -1,43 +1,28 @@
-#generate permutation 
+#generate permutation
 import spglib
 import numpy as np
 import pymatgen
 from copy import deepcopy
-number_of_colors = 2        #exist or not exist
-o_sublattice = ([(4, 0, 0),
 
-           (0, 4, 0),
+def gene_per(rotation, translation, dic_zahyou):
+    """
+    対象操作について置換の移り先を示す辞書を作成
 
-           (0, 0, 6)],
+    parameters
 
-          [(0.3, 0.3, 0.0),
+    retruns
 
-           (0.7, 0.7, 0.0),
-
-           (0.2, 0.8, 0.25),
-
-           (0.8, 0.2, 0.25),
-           (0.3, 0.3, 0.5),(0.7, 0.7, 0.5),(0.2, 0.8, 0.75),(0.8, 0.2, 0.75)],
-
-          [8, 8, 8, 8, 8, 8, 8, 8])
-o_subsim = spglib.get_symmetry(o_sublattice)
-o_subspacegroup = spglib.get_spacegroup(o_sublattice)
-arr = np.asarray(o_sublattice[1])   #lattcie no 行列表示
-dic = dict()                        #分率座標の行列表示
-for i in range(len(o_sublattice[1])):
-    dic[i] = np.asarray(o_sublattice[1][i])
-#移り先を示す置換を表現　{0: 2, 1: 3, 2: 1, 3: 0, 4: 6, 5: 7, 6: 5, 7: 4}
-def gene_per(i):
-    rot = np.asarray(o_subsim["rotations"][i])
-    dic2 = deepcopy(dic)
-    for l in range(8):
-        dic2[l] = (dic2[l].dot(rot) + o_subsim["translations"][i])%1
+    """
+    dic2 = deepcopy(dic_zahyou)
+    for l in range(len(dic_zahyou)):
+        dic2[l] = (rotation.dot(dic_zahyou[l]) + translation) % 1
     tin = dict()
-    for l in range(len(o_sublattice[1])):
-        for j in range(len(o_sublattice[1])):
-            if all(np.round(dic[j], 4) == np.round(dic2[l], 4)):
+    for l in range(len(dic_zahyou)):
+        for j in range(len(dic_zahyou)):
+            if all(np.round(dic_zahyou[j], 4) == np.round(dic2[l], 4)):
                 tin[l] = j
     return tin
+
 def generate_per(superlattice, o_sublattice):
     parent_sym = spglib.get_symmetry(superlattice)
     tin = dict()
@@ -45,23 +30,10 @@ def generate_per(superlattice, o_sublattice):
     dic = dict()                        #分率座標の行列表示
     for i in range(len(o_sublattice[1])):
         dic[i] = np.asarray(o_sublattice[1][i])
-    
-    def gene_per(i):
-        rot = np.asarray(parent_sym["rotations"][i])
-        dic2 = deepcopy(dic)       # taishousousa no ato no zahyou
-        for l in range(len(o_sublattice[2])):
-            dic2[l] = (rot.dot(dic2[l]) + parent_sym["translations"][i])%1
-        tin = dict()
-        for l in range(len(o_sublattice[1])):
-            for j in range(len(o_sublattice[1])):
-                if all(np.round(dic[j], 4) == np.round(dic2[l], 4)):
-                    tin[l] = j
-        return tin
-    
+
     jun = dict()
     for i in range(len(parent_sym["translations"])):
-        tin = gene_per(i)
-        jun[i] = tin
+        jun[i] = gene_per(parent_sym["rotations"][i], parent_sym["translations"][i], dic)
     return jun
 
 # 置換の積の式を作る [{0, 1, 2, 3}, {4, 5, 6, 7}]
@@ -98,11 +70,11 @@ def polya(jun):
     polya = sum / len(jun)
     return polya
 
-#unique 
-def unique(parent_sym_jun, omomi4):
+#unique
+def unique(o_sublattice, parent_sym_jun, omomi4):
     omomi4_neo = deepcopy(omomi4)#copy wo sakusei
     lis = set()
-    
+
     for i in range(len(omomi4)):#through all candidate
         if omomi4[i] in omomi4_neo:#kouho ga mada 生き残ってるかチェック
             for j in range(1, len(parent_sym_jun)):#置換操作について回す　
@@ -135,9 +107,9 @@ def make_candidate(o_sublattice, l):
         for m in range(len(k)):
             sum += int(k[m])
         if sum ==l :
-            omomi4.append(k) 
+            omomi4.append(k)
     return omomi4
 
-                                                                                                                      
-    
-    
+
+
+
